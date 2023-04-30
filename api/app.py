8c57b20ad4e54d51bf8e58264b02ac1a2ad5617d
burcu arslan <burcuarslln@gmail.com>
2023-04-30 17:47:22 +0300

    yasandi bitti saygisizca.. daha guzel yapardim ama vaktim yoktu :( nt

--- api/app.py
+++ api/app.py
@@ -4,11 +4,16 @@ import cv2
 import numpy as np
 from flask import Flask, request, jsonify, make_response
 from PIL import Image
+import base64
 
 app = Flask(__name__)
 
+from flask import Flask, request
+import numpy as np
+import cv2
+import base64
 
-
+app = Flask(__name__)
 
 @app.route('/process_image', methods=['POST'])
 def process_image():
@@ -27,11 +32,13 @@ def process_image():
 def cropped_image():
     data = request.form
 
-    w = int(data.get('w'))
-    h = int(data.get('h'))
-    y = int(data.get('y'))
-    x = int(data.get('x'))
+    w = int(float(data.get('w')))
+    h = int(float(data.get('h')))
+    y = int(float(data.get('y')))
+    x = int(float(data.get('x')))
+
     img = cv2.imdecode(np.fromstring(request.files['image'].read(), np.uint8), cv2.IMREAD_UNCHANGED)
+
     # Boyutlandırma
     # resized_img = cv2.resize(img, (w,h))
     cropped_image = img[y:y + h, x:x + w]
@@ -49,6 +56,7 @@ def rotate_image():
 
 def buffer(img):
     # PIL Image nesnesi oluşturma
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     output_img = Image.fromarray(img)
     # JPEG formatına dönüştürme ve response nesnesi içinde kullanma
     output_buffer = BytesIO()
